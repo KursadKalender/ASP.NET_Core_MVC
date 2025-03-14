895ee38b254f0bf10aa66380203bd73915229774
Kursad <Kursad@DESKTOP-TBPARUJ>
2025-03-14 16:09:23 +0300

    14.03.2025
    1.Category, Comment, Role ve User sınıfları için mapping işlemi tamamlandı.
    2.DbContext içerisinde Map Configurationları uygulandı.
    3.UnitOfWork interfacesi oluşturuldu, SaveAsync metodu tanımlandı.
    4.İlk Role ve Rser nesnesi oluşturuldu.

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork: IAsyncDisposable // Tüm repositoryleri tek bir yerden yönetebilmemizi sağlıyor. İşlem yaparken tüm repositoryleri newlemek yerine sadece UnitOfWork newlenir.
+    {
+        // Transaction yapısına sahiptir = Veritabanına gönderdiğimiz verilerin doğrulanması ve yönetimi. 
+
+        IArticleRepository Articles { get; } // unitofwork.Articles
+        ICategoryRepository Categories { get; } //unitofwork.Categories
+        ICommentRepository Comments { get; } //unitofwork.Comments
+        IRoleRepository Roles { get; } //unitofwork.Roles
+        IUserRepository Users { get; } //unitofwork.Users
+
+        // _unitOfWork.Categories.AddAsync(category);
+        // _unitOfWork.Users.AddAsync(user);
+        // _unitOfWork.SaveAsync();
+
+        Task<int> SaveAsync();
+    }
+}
---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/UserMap.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.ID);
+            builder.Property(u => u.ID).ValueGeneratedOnAdd();
+            builder.Property(u => u.Email).IsRequired();
+            builder.Property(u => u.Email).HasMaxLength(50);
+            builder.HasIndex(u => u.Email).IsUnique(); // Değer unique mi?
+            builder.Property(u => u.Username).IsRequired();
+            builder.Property(u => u.Username).HasMaxLength(20);
+            builder.HasIndex(u => u.Username).IsUnique();
+            builder.Property(u => u.PasswordHash).IsRequired();
+            builder.Property(u => u.PasswordHash).HasColumnType("VARBINARY(500)");
+            builder.Property(u => u.Description).HasMaxLength(500);
+            builder.Property(u => u.FirstName).IsRequired();
+            builder.Property(u => u.FirstName).HasMaxLength(30);
+            builder.Property(u => u.LastName).IsRequired();
+            builder.Property(u => u.LastName).HasMaxLength(30);
+            builder.Property(u => u.Picture).IsRequired();
+            builder.Property(u => u.Picture).HasMaxLength(250);
+            builder.Property(u => u.CreatedByName).IsRequired(true);
+            builder.Property(u => u.CreatedByName).HasMaxLength(50);
+            builder.Property(u => u.ModifiedByName).IsRequired(true);
+            builder.Property(u => u.ModifiedByName).HasMaxLength(50);
+            builder.Property(u => u.CreatedDate).IsRequired(true);
+            builder.Property(u => u.ModifiedDate).IsRequired(true);
+            builder.Property(u => u.IsActive).IsRequired(true);
+            builder.Property(u => u.IsDeleted).IsRequired(true);
+            builder.Property(u => u.Note).HasMaxLength(500);
+            builder.HasOne<Role>(u => u.Role).WithMany(u => u.Users).HasForeignKey(u => u.RoleID);
+            builder.ToTable("Users");
+
+            builder.HasData(new User
+            {
+                ID = 1,
+                RoleID = 1,
+                FirstName = "Kursad",
+                LastName = "Kalender",
+                Username = "kursadkalender",
+                Email = "kursad@kalender.dev",
+                IsActive = true,
+                IsDeleted = false,
+                CreatedByName = "InitialCreate",
+                CreatedDate = DateTime.Now,
+                ModifiedByName = "InitialCreate",
+                ModifiedDate = DateTime.Now,
+                Description = "İlk Admin Kullanıcı",
+                Note = "Admin Kullanıcı",
+                PasswordHash = Encoding.ASCII.GetBytes("0192023a7bbd73250516f069df18b500") //MD5 Hashed Password-Admin123
+            });
+        }
+    }
+}
